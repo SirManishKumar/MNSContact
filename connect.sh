1e7bab69a9f016ca8e57c99295fa0ddf3831925c
Manish Kumar Chaturvedi <79128476+SirManishKumar@users.noreply.github.com>
2022-07-15 12:46:31 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -194,16 +194,17 @@ echo "         Created By:- https://github.com/SirManishKumar " | lolcat
 echo "      Follow On Twitter:- http://twtter.com/SirManishKumar " | lolcat
 echo " ╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯" | lolcat
 echo -e "\e[1;36m"
-echo ' 1. Update  MNSContect          ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮'
-echo ' 2. Restart MNSContect         |         u. Update          |'
-echo ' 3. Contect By YouTube         |         r. Restart         |'
-echo ' 4. Contect By Facebook        |         g. Github          |'
-echo ' 5. Contect By Twitter         |         y. Youtube         |'
-echo ' 6. Contect By WhatsApp        |         f. Facebook        |'
-echo ' 7. Contect By Github          |         t. Twitter         |'
-echo ' 8. Contect By Call            |         w. WhatsApp        |'
-echo ' 9. Contect By SMS             |  github.com/SirManishKumar |'
-echo '10. Contect By Massenger       ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯'
+echo ' 1. Update  MNSContect         ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮'
+echo ' 2. Restart MNSContect         |       Need To Update       |'
+echo ' 3. Contect By YouTube         |       Need To Restart      |'
+echo ' 4. Contect By Facebook        |         Go To Github       |'
+echo ' 5. Contect By Twitter         |      Check On Youtube      |'
+echo ' 6. Contect By Github          |      Check On Facebook     |'
+echo ' 7. Contect By Call            |     Follow On Twitter      |'
+echo ' 8. Contect By SMS             |    Contect By WhatsApp     |'
+echo ' 9. Contect By WhatsApp        |    Contect By Website      |'
+echo '10. Contect By Messenger       |  github.com/SirManishKumar |'
+echo '11. Contect By Website         ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯'
 echo '                                           0. Exit '
 echo "=============================================================" | lolcat
 echo "            Please Choose Your Number To Contact Us " | lolcat
@@ -211,7 +212,7 @@ echo "=============================================================" | lolcat
 echo -e "\e[1;32m╭─[ Enter Number To Contect ]"
 read -p "╰──> " pil;
 case $pil in
-u) echo " Contacting To Server Please Wait.............................💯" | lolcat
+1) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Update MNSContect Please Wait.                     " | lolcat
    apt update && apt upgrade
    apt install git -y
@@ -220,269 +221,67 @@ u) echo " Contacting To Server Please Wait.............................💯" | l
    clear
 
 ;;
-w) echo " Contacting To Server Please Wait.............................💯" | lolcat
+9) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Connecting By WhatsApp Please Wait.                   " | lolcat
    xdg-open https://wa.me/+919554988808?text=*From%20MNSUpgrade-*%20I%20Have%20To%20Ask%20For%20
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-f) echo " Contacting To Server Please Wait.............................💯" | lolcat
+4) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Open Facebook Please Wait.                         " | lolcat
    xdg-open https://facebook.com/sirmanishkumar
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-y) echo " Contacting To Server Please Wait.............................💯" | lolcat
+3) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Open Youtube Please Wait.                          " | lolcat
    xdg-open https://youtube.com/c/manishkumarmns
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-t) echo " Contacting To Server Please Wait.............................💯" | lolcat
+5) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Open Twitter Please Wait.                          " | lolcat
    xdg-open https://twitter.com/sirmanishkumar
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-g) echo " Contacting To Server Please Wait.............................💯" | lolcat
+6) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Open Github Please Wait.                           " | lolcat
    xdg-open https://github.com/SirManishKumar
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-c) echo " Contacting To Server Please Wait.............................💯" | lolcat
+7) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Connect Call Please Wait.                           " | lolcat
    xdg-open tel:+919554988808
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-s) echo " Contacting To Server Please Wait.............................💯" | lolcat
+8) echo " Contacting To Server Please Wait.............................💯" | lolcat
    echo " Starting To Connect SMS Please Wait.                           " | lolcat
    xdg-open sms://+919554988808?body=You%20Have%20found%20Any%20Mistake%20In%20Our%20Github%20Tools.%20Please%20Ask%20Your%20Question.?
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-M) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   echo " Starting To Connect SMS Please Wait.                           " | lolcat
+10) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Connect Messenger Please Wait.                           " | lolcat
    xdg-open https://m.me/Sirmanishkumar
    echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
 
 ;;
-1) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSAllTools Please Wait.                   " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf MNSAllTools
-   git clone https://github.com/SirManishKumar/MNSAllTools
-   mv MNSAllTools $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSAllTools
-   chmod 777 *
-   bash installer.sh
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
+11) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Connect Website Please Wait.                           " | lolcat
+   xdg-open https://akhandramayan.wordpress.com
+   echo " It's finished Thank you for using MNSContect                   " | lolcat
    clear
-   exit
-
-;;
-2) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSTools Please Wait.                      " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf MNSTools
-   git clone https://github.com/SirManishKumar/MNSTools
-   mv MNSTools $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSTools
-   chmod 777 install.py
-   python install.py
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-3) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSLock Please Wait.                       " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   git clone https://github.com/SirManishKumar/MNSLock
-   mv MNSLock $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSLock
-   chmod 777 install.sh
-   bash install.sh
-   cd && rm -rf MNSLock
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-4) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSNgrok Please Wait.                      " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   cd && rm -rf ngrok
-   git clone https://github.com/SirManishKumar/MNSNgrok
-   mv MNSNgrok $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSNgrok
-   chmod 777 setup.sh
-   bash setup.sh
-   cd && rm -rf MNSNgrok
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-5) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSUbuntu Please Wait.                     " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   cd && rm -rf MNSUbuntu
-   git clone https://github.com/SirManishKumar/MNSUbuntu
-   mv MNSUbuntu $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSUbuntu
-   chmod +x ubuntu.sh
-   ./ubuntu.sh
-   ./startubuntu.sh
-   exit
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-6) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSSnake Please Wait.                      " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf MNSSnake
-   git clone https://github.com/SirManishKumar/MNSSnake
-   mv MNSSnake $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSSnake
-   chmod 777 snake.py
-   python snake.py
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-7) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSTermux Please Wait.                     " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf MNSTermux
-   git clone https://github.com/SirManishKumar/MNSTermux
-   mv MNSTermux $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSTools
-   chmod 777 setup.sh
-   bash setup.sh
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-8) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade TermuxLock Please Wait.                    " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   git clone https://github.com/SirManishKumar/TermuxLock
-   mv TermuxLock $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd TermuxLock
-   chmod 777 install.sh
-   bash install.sh
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-9) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade TermuxTool Please Wait.                    " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf TermuxTool
-   git clone https://github.com/SirManishKumar/TermuxTool
-   mv TermuxTool $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd TermuxTool
-   chmod 777 setup.sh
-   bash setup.sh
-   echo " It's finished Thank you for using MNSUpgrade                   " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
-
-;;
-10) echo " Contacting To Server Please Wait.............................💯" | lolcat
-   sleep 1
-   echo " Starting To Upgrade MNSBasicTool Please Wait.                   " | lolcat
-   termux-setup-storage
-   apt update && apt upgrade -y
-   apt install git -y
-   apt install python -y
-   cd && rm -rf MNSBasicTool
-   git clone https://github.com/SirManishKumar/MNSBasicTool
-   mv MNSBasicTool $HOME
-   cd && rm -rf MNSUpgrade
-   cd && cd MNSBasicTool
-   chmod 777 Tools.py
-   python Tools.py
-   echo " It's finished Thank you for using MNSUpgrade                    " | lolcat
-   figlet -f slant "MNSUpgrade" | lolcat
-   sleep 1
-   clear
-   exit
 
 ;;
 0) echo "Author : Manish Kumar Chaturvedi" | lolcat
